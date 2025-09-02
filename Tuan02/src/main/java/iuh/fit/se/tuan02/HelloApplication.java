6f8b19bb19ea84780723f6768285841e7c7527ff
TrungHan <letrunghan1692003@gmail.com>
2025-09-02 11:24:48 +0700

    Tuan02

---
+++ Tuan02/src/main/java/iuh/fit/se/tuan02/HelloApplication.java
@@ -0,0 +1,9 @@
+package iuh.fit.se.tuan02;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
